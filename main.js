8bdaf96f7f61fe057d7b6634f056b713419ba4ff
fotohabilaj <fotohabilaj@gmail.com>
2026-02-09 17:16:50 +0100

    odd even complete

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ if(parola===alorap){
 const parola = 'aia';
 
 palindetector(parola)
+
+
+
+const even= true;
+const numb= 2;
+
+const pc_rndm= Math.floor(Math.random()*5+1);
+const sum=pc_rndm+numb;
+console.log(pc_rndm,numb);
+
+function odd_or_even_game(num){
+    const number= num;
+    if(num % 2===0){
+        console.log(number,'è pari');
+        if(even==true){console.log('hai vinto');
+        } else{console.log('il pc vince')}
+    }  else {console.log(number,'è dispari');
+        if(even==false){console.log('hai vinto');
+        } else{console.log('il pc vince')}
+    }
+}
+odd_or_even_game(sum)
+
