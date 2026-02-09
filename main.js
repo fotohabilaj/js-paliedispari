c6e6ac6717a6d6cb2b2c4378120aa2c8935dc3b0
fotohabilaj <fotohabilaj@gmail.com>
2026-02-09 16:54:38 +0100

    palindetector complete

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 console.log('vamos');
 
-const parola = 'aia';
+
+
+function palindetector(str){
+    
+const parola = str;
 let alorap= []
 for(let i= parola.length-1 ; i>=0; i--){
     const lettera= parola[i];
     alorap= alorap + lettera
     
 }
-console.log(alorap);
+
 if(parola===alorap){
     console.log(parola,'=',alorap,' palindromo rilevato');
     
 } else {console.log('palindromo non rilevato');
 }
 
-
-function palindetector(parola){
-    
-    
 }
 
 
+const parola = 'aia';
+
+palindetector(parola)
