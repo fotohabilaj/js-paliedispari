dcbafd2fd83a1ce7759f3e6582ed056c2a83d19c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-09 16:46:35 +0100

    add for cicle an if else

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 console.log('vamos');
 
-const parola = 'gatto';
-
-function palindetector(){
+const parola = 'aia';
+let alorap= []
+for(let i= parola.length-1 ; i>=0; i--){
+    const lettera= parola[i];
+    alorap= alorap + lettera
     
 }
+console.log(alorap);
+if(parola===alorap){
+    console.log(parola,'=',alorap,' palindromo rilevato');
+    
+} else {console.log('palindromo non rilevato');
+}
+
+
+function palindetector(parola){
+    
+    
+}
+
+
